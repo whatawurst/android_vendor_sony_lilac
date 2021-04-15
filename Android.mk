d086edbe144714d4365b3f0d7a69c4e80601a8c2
Alexander Grund <Flamefire@users.noreply.github.com>
2021-04-15 21:56:20 +0200

    Revert "Remove Sony stock camera app"
    This reverts commit 556176f1a6f57fcf6f5aaeccd5944f39a794e779.

--- Android.mk
+++ Android.mk
@@ -6,4 +6,13 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),lilac)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := SemcCameraUI-xxhdpi-release
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := proprietary/priv-app/SemcCameraUI-xxhdpi-release/SemcCameraUI-xxhdpi-release.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PREBUILT)
+
 endif
